eae859e94b37c938450e35d5534563aff135cc25
CodeNewbieee <choi2232@hanmail.net>
2024-01-08 19:54:10 +0900

    add: 레이아웃2차 완성, 애니메이션 효과 추가

--- app/src/main/java/com/example/applemarket/SecondActivity.kt
+++ app/src/main/java/com/example/applemarket/SecondActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.applemarket.databinding.ActivityMainBinding
 import com.example.applemarket.databinding.ActivitySecondBinding
+import java.text.DecimalFormat
 
 class SecondActivity : AppCompatActivity() {
     private val binding by lazy { ActivitySecondBinding.inflate(layoutInflater) }
@@ -11,6 +12,16 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.ivItemBackbtn.bringToFront()
+        with(binding) {
+            ivItemBackbtn.bringToFront() // 이미지 최상단 불러오기
+            // 뒤로가기 클릭시 화면 종료 및 메인 페이지
+            ivItemBackbtn.setOnClickListener {
+                if(!isFinishing) finish()
+            }
+            // 숫자 콤마 추가
+            val decimal = DecimalFormat("#,###")
+            tvItemPrice.text="${decimal.format(1800000)}원"
+        }
+
     }
 }
