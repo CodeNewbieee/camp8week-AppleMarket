6d12e8ebe2e96460f30b52dc3f967bb7a83d5025
CodeNewbieee <choi2232@hanmail.net>
2024-01-08 16:30:05 +0900

    add : 상품 및 상세페이지 레이아웃

---
+++ app/src/main/java/com/example/applemarket/Merchandise.kt
@@ -0,0 +1,4 @@
+package com.example.applemarket
+
+data class Merchandise (val photo: Int, val name: String, val intro: String, val seller: String,
+                        val price: Int, val address: String, val good:Int, val chat: Int )
---
+++ app/src/main/java/com/example/applemarket/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.example.applemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.applemarket.databinding.ActivityMainBinding
+import com.example.applemarket.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySecondBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
